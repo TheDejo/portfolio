ed3ff68c08142032849852d6cac2f010eb1bc42c
TheDejo <65681287+TheDejo@users.noreply.github.com>
2020-11-01 17:25:01 +0100

    deploying my portfolio

---
+++ build/precache-manifest.1ce2eec9e8c530883271a6e19a56897a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b02a412596c0079bc112b91a1bbec786",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "48c9e73b8b90055ffb5e",
+    "url": "/portfolio/static/css/main.753b75a1.chunk.css"
+  },
+  {
+    "revision": "db704988856a584ed9b4",
+    "url": "/portfolio/static/js/2.2250556e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.2250556e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48c9e73b8b90055ffb5e",
+    "url": "/portfolio/static/js/main.08aaeddb.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "d6ad4986f624fc59580bf446ba87e6cd",
+    "url": "/portfolio/static/media/Cover.d6ad4986.png"
+  },
+  {
+    "revision": "8680c54e7ea7379ff6e82504d60c7a8c",
+    "url": "/portfolio/static/media/js.8680c54e.png"
+  },
+  {
+    "revision": "e84c2f03ada9047bae228e5980c67834",
+    "url": "/portfolio/static/media/logo.e84c2f03.png"
+  },
+  {
+    "revision": "6ee58e27d94f19f7a837d99c984a2bf9",
+    "url": "/portfolio/static/media/ogbe.6ee58e27.png"
+  }
+]);
